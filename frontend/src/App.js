a4fc71338f12097fcac25ad0e291abcfd66cf11a
Ruvini Senevirathna <ruviniruvi@gmail.com>
2022-08-06 16:52:24 -0400

    Frontend files added

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
