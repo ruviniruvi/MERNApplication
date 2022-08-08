8698b6a8157cb5c566c44e89d6cae0e5212bc9ce
Ruvini Senevirathna <ruviniruvi@gmail.com>
2022-08-08 11:44:31 -0400

    changes in App.js file and Home.js file

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react"
+
+
 const Home = () =>{
+//fetch data from backend
+    useEffect(()=>{
+const fetchWorkouts = async () =>{
+const response = await fetch('http://localhost:4000/api/workouts')
+}
+fetchWorkouts()
+})
+
     return (
         <div classname="home">
             <h2>Home Page</h2>
